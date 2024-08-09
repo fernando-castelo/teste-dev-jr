5702ab2dc5ef7e45098a2c94e00ad50180a33705
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-08-09 15:58:54 -0300

    feat(): add user fields validation

--- backend/src/user/dto/createUserDto.ts
+++ backend/src/user/dto/createUserDto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
+  @IsString({ message: 'Campo nome deve ser String' })
+  @IsNotEmpty({ message: 'Campo nome deve ser preenchido' })
   nomeCompleto: string;
 
-  @IsString()
+  @IsEmail({}, { message: 'Campo email deve estar em formato valido' })
+  @IsNotEmpty({ message: 'Campo email deve ser preenchido' })
   email: string;
 }
